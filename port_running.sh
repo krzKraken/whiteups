d8cd2750e27da0a73567f75fb802b132c08ca408
Parallels <parallels@kali-linux-2022-2.localdomain>
2023-02-09 22:16:19 -0500

    Script puertos y servicios corriendo

--- port_running.sh
+++ port_running.sh
@@ -1,1 +1,26 @@
-echo;echo "$(cat /proc/net/tcp | awk '{print $2}' | cut -d ':' -f 2  | tr -d 'local_address' | xargs | tr ' ' '\n')" | sort -u| while read line; do echo "[+] Puerto $line -> $(echo "obase=10; ibase=16; $line" | bc) -open"; done
+#!/bin/bash
+
+function ctrl_c(){
+  echo "\n\n [!] Saliendo..."
+  exit 1
+}
+
+#Ctrl+c
+trap ctrl_c INT
+
+echo "\n[+] Los puertos abiertos son los siguientes:\n"
+
+
+function hex_to_dec(){
+  hex="$1"
+  echo "obase=10; ibase=16; $hex" | bc
+}
+
+function which_process_in_port(){
+  port=$1
+  echo "$(lsof -i:$port | awk '{print $1}' | xargs | awk 'NF{print $NF}')"
+}
+
+echo "$(cat /proc/net/tcp | awk '{print $2}' | cut -d ':' -f 2  | tr -d 'local_address' | xargs | tr ' ' '\n')" | sort -u| while read line; do echo "[+] Puerto $line -> $(hex_to_dec $line) -open -> Ejecutando:  $(which_process_in_port $(hex_to_dec $line) )"; done
+
+# Agregar lsof -i: <Puerto> para conocer el nombre del proceso ejecutandose por ese puerto
