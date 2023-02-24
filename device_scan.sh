2db48b63fa81db665a11caaac4b619557ef5b288
krzkraken <krzkraken@localhost.localdomain>
2023-02-24 06:06:51 -0500

    inicio script buscador en contenido url

--- device_scan.sh
+++ device_scan.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# Variables Globales
+
+
+
 function ctrl_c(){
   echo -e "\n\n [!] Saliendo..."
   tput cnorm; exit 1
 }
 
 function wich_lan(){
- red=$(ifconfig eth0 | grep 'inet ' | awk '{print $2}')
+ lan=$(echo $(ip a | grep ens33 | tail -n 1 | awk '{print $2}' | cut -d '/' -f 1 | tr '.' '\n' | head -n 3 | tr '\n' '.' )"x" )
+ echo "$lan"
 }
 
 #Ctrl+c
@@ -14,10 +19,11 @@ trap ctrl_c INT
 
 
 echo -e "\n[!] Iniciando Escaneo de Dispositivos en la red: \n"
-
+echo -e "\n[!] Buscando en rango de red:"
+wich_lan
 
 tput civis #-> Oculta el Cursor
 for i in $(seq 1 254); do
-  (timeout 1 bash -c "ping -c 1 192.168.1.$i") &>/dev/null && echo "Dispositivo en la Ip: 192.168.1.$i - Activo" &
+  (timeout 1 bash -c "ping -c 1 192.168.100.$i") &>/dev/null && echo "Dispositivo en la Ip: 192.168.1.$i - Activo" &
 done; wait
 tput cnorm # -> Recupera el cursorwq
--- machine_finder.sh
+++ machine_finder.sh
@@ -11,38 +11,55 @@ turquoiseColour="\e[0;36m\033[1m"
 grayColour="\e[0;37m\033[1m"
 
 # Variables Globales
-main_url="https://htbmachins.hithub.io/bundle.js"
+main_url="https://htbmachines.github.io/bundle.js"
 
 
 # Handle error
-function ctrl_c(){
+ctrl_c(){
  echo -e "${redColour}\n\n[+] Saliendo...${endColour}"
-  tput cnorm;exit 1
+  tput cnorm && exit 1
 }
 
-function helpPanel(){
+helpPanel(){
   echo -e "\n${yellowColour}[+]${endColour}Uso:\n"
   echo -e "\t${purpleColour}u)${endColour} Descargar o Actualizar archivos necesarios"
   echo -e "\t${purpleColour}m)${endColour} Buscar por Nombre de Maquina."
   echo -e "\t${purpleColour}h)${endColour} Mostrar este panel de ayuda."
 }
 
-function searchMachine(){
-  echo "$1"
+searchMachine(){
+  machine_name="$1"
+  echo -e "${yellowColour}[+]${endColour}${greenColour} Buscando coincidencias para: $machine_name${endColour} \n"
+  cat bundle.js | awk "/name: \"Tentacle\"/,/resuelta:/" | grep -vE "id|sku" | sed "s/^ *//"
 }
 
-val="$(cat bundle.js; echo $?)"
-echo "$val"
-
-function updateFiles (){
- if [ "$]; do
-   echo "Archivo existe"
- else
-   echo "Archivo no Existe"    
-
-#curl -s $"main_url" > bundle.js
-
-}
+updateFiles (){
+  
+  if [ ! -f bundle.js ];then
+    tput civis
+    echo -e "\n${yellowColour}[+]${endColour}${greenColour} Descargando archivos necesarios..${endColour}"
+    curl -s $main_url > bundle.js
+    js-beautify bundle.js | sponge bundle.js
+    echo -e "\n${yellowColour}[+]${endColour}${greenColour} Todos los archivos se cargaron correctamente ${endColour}${redColour}:)${endColour}"
+    tput cnorm
+  else
+    tput civis
+    echo -e "\n${yellowColour}[!]${endColour}${greenColour}Verificando Archivos...${endColour}"
+    curl -s $main_url > bundle_temp.js
+    js-beautify bundle_temp.js | sponge bundle_temp.js
+    md5sum_original_value="$(md5sum bundle.js | awk '{print $1}')"
+    md5sum_temp_value="$(md5sum bundle_temp.js | awk '{print $1}')"
+    tput cnorm
+    if [ "$md5sum_original_value" == "$md5sum_temp_value" ]; then
+      echo -e "\n${yellowColour}[+]${endColour}${greenColour} No Hay actualizaciones disponibles...${endColour}"
+      rm bundle_temp.js
+    else
+      echo -e "\n${yellowColour}[+]${endColour}${greenColour} Actualizando archivos...${endColour}"
+      rm bundle.js; mv bundle_temp.js bundle.js
+      echo -e "\n${yellowColour}[+]${endColour}${grayColour} Todos los archivos se han actualizado correctamente.${endColour}"
+    fi
+  fi
+} 
 
 # Indicadores
 declare -i parameter_counter=0
